7b25c4d1474bf2a427982c60731f10659ccc1245
xlho99 <105140721+xlho99@users.noreply.github.com>
2022-06-04 09:36:00 +0800

    Stores searching

---
+++ SearchStore.java
@@ -0,0 +1,35 @@
+package oop2022FinalProject;
+
+/**
+ * 
+ * @author xlho99
+ * @version ver.0
+ * @changes
+ * added: 
+ * changed: 
+ * 
+ */
+    
+    public class SearchStore {
+    	private String name;
+        private String type;
+        
+
+        public boolean matchCriteria(String name, String type)
+        {
+            return matchesName(name) && matchesType(type);
+        }
+
+        private boolean matchesName(String name){
+            return name.equals("") || name.equals(this.name) || this.name.indexOf(name) >= 0;
+        }
+
+        private boolean matchesType(String type) {
+            return type.equals("") || this.type.getType().equals(type);
+        }
+
+        
+    }
+    
+
+
